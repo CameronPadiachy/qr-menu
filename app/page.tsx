9d7a75d1c84816a4f3d515c95e73eb16521c52d5
CameronPadiachy <cameronpadiachy@icloud.com>
2026-01-22 20:00:59 +0200

    mobile phone compatabillity

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
 export default function Home() {
   return (
-    <iframe
-      src="/menu.pdf"
-      style={{ width: "100vw", height: "100vh", border: "none" }}
-    />
+    <div style={{ height: '100vh' }}>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer fileUrl="/menu.pdf" />
+      </Worker>
+    </div>
   );
 }
